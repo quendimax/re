3be4d1452d25d60a1fcca97b7070545c4db098fb
Maksim Tomkavich <quendimax@gmail.com>
2025-02-02 20:22:27 +0100

    feat: add forward/backward methods to Symbol trait

--- regr/src/symbol.rs
+++ regr/src/symbol.rs
@@ -3,8 +3,21 @@
 /// It could be based on [`std::iter::Step`] trait, but it is experimental and
 /// is not available within Rust's stable standard library.
 pub trait Symbol: Sized + Copy {
-    /// Returns the number of steps required to get from `self` to `other` or vice versa.
+    /// Returns the number of steps required to get from `self` to `other` or
+    /// vice versa.
     fn steps_between(&self, other: Self) -> usize;
+
+    /// Returns the value that would be obtained by taking the _successor_ of
+    /// `self` count times.
+    ///
+    /// Returns `None` if the value is out of `Self`'s valid range.
+    fn forward(&self, count: usize) -> Option<Self>;
+
+    /// Returns the value that would be obtained by taking the _predecessor_ of
+    /// `self` count times.
+    ///
+    /// Returns `None` if the value is out of `Self`'s valid range.
+    fn backward(&self, count: usize) -> Option<Self>;
 
     /// Checks if there is one step between the specified two values.
     fn adjoins(&self, other: Self) -> bool {
@@ -16,11 +29,43 @@ impl Symbol for u8 {
     fn steps_between(&self, other: Self) -> usize {
         self.abs_diff(other).into()
     }
+
+    fn forward(&self, count: usize) -> Option<Self> {
+        if let Ok(count) = Self::try_from(count) {
+            self.checked_add(count)
+        } else {
+            None
+        }
+    }
+
+    fn backward(&self, count: usize) -> Option<Self> {
+        if let Ok(count) = Self::try_from(count) {
+            self.checked_sub(count)
+        } else {
+            None
+        }
+    }
 }
 
 impl Symbol for u32 {
     fn steps_between(&self, other: Self) -> usize {
         self.abs_diff(other) as usize
+    }
+
+    fn forward(&self, count: usize) -> Option<Self> {
+        if let Ok(count) = Self::try_from(count) {
+            self.checked_add(count)
+        } else {
+            None
+        }
+    }
+
+    fn backward(&self, count: usize) -> Option<Self> {
+        if let Ok(count) = Self::try_from(count) {
+            self.checked_sub(count)
+        } else {
+            None
+        }
     }
 }
 
@@ -33,5 +78,31 @@ impl Symbol for char {
             steps -= 0xDFFF - 0xD800 + 1;
         }
         steps as usize
+    }
+
+    fn forward(&self, count: usize) -> Option<Self> {
+        let start = *self as u32;
+        let mut res = start.forward(count)?;
+        if start < 0xD800 && 0xD800 <= res {
+            res = res.forward(0x800)?;
+        }
+        if res <= char::MAX as u32 {
+            // SAFETY: res is a valid unicode scalar
+            // (below 0x110000 and not in 0xD800..0xE000)
+            Some(unsafe { char::from_u32_unchecked(res) })
+        } else {
+            None
+        }
+    }
+
+    fn backward(&self, count: usize) -> Option<Self> {
+        let start = *self as u32;
+        let mut res = start.backward(count)?;
+        if start >= 0xE000 && 0xE000 > res {
+            res = res.backward(0x800)?;
+        }
+        // SAFETY: res is a valid unicode scalar
+        // (below 0x110000 and not in 0xD800..0xE000)
+        Some(unsafe { char::from_u32_unchecked(res) })
     }
 }
--- regr/tests/regr/symbol.rs
+++ regr/tests/regr/symbol.rs
@@ -9,6 +9,18 @@ fn u8_steps_between() {
 }
 
 #[test]
+fn u8_forward() {
+    assert_eq!(1u8.forward(3), Some(4));
+    assert_eq!(1u8.forward(1000), None);
+}
+
+#[test]
+fn u8_backward() {
+    assert_eq!(1u8.backward(1), Some(0));
+    assert_eq!(1u8.backward(2), None);
+}
+
+#[test]
 fn u8_adjoins() {
     assert!(1u8.adjoins(2));
     assert!(1u8.adjoins(0));
@@ -21,6 +33,18 @@ fn u32_steps_between() {
     assert_eq!(1u32.steps_between(2), 1);
     assert_eq!(1u32.steps_between(1), 0);
     assert_eq!(8u32.steps_between(2), 6);
+}
+
+#[test]
+fn u32_forward() {
+    assert_eq!(1u32.forward(3), Some(4));
+    assert_eq!(1u32.forward(u32::MAX as usize), None);
+}
+
+#[test]
+fn u32_backward() {
+    assert_eq!(1u32.backward(1), Some(0));
+    assert_eq!(1u32.backward(2), None);
 }
 
 #[test]
@@ -37,6 +61,20 @@ fn char_steps_between() {
     assert_eq!('a'.steps_between('a'), 0);
     assert_eq!('c'.steps_between('a'), 2);
     assert_eq!('\u{D7FF}'.steps_between('\u{E000}'), 1);
+}
+
+#[test]
+fn char_forward() {
+    assert_eq!('a'.forward(2), Some('c'));
+    assert_eq!('a'.forward(0x10FFFF), None);
+    assert_eq!('\u{D7FF}'.forward(1), Some('\u{E000}'));
+}
+
+#[test]
+fn char_backward() {
+    assert_eq!('b'.backward(1), Some('a'));
+    assert_eq!('a'.backward(0x10FFFF), None);
+    assert_eq!('\u{E000}'.backward(1), Some('\u{D7FF}'));
 }
 
 #[test]
