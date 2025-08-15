7bba10a0688b4cbaa8cd1f44d656d4cc9e16af7b
Maksim Tomkavich <quendimax@gmail.com>
2025-08-16 00:55:22 +0200

    feat(resy): impl Display for Hir

--- resy/src/hir.rs
+++ resy/src/hir.rs
@@ -1,4 +1,5 @@
-use redt::SetU8;
+use redt::{Legible, SetU8};
+use std::fmt::Write;
 
 /// HirKind represents a high-level intermediate representation of a regular
 /// expression, that contains already encoded into bytes unicode code points,
@@ -73,6 +74,26 @@ impl Hir {
         Hir::Literal(bytes.to_vec())
     }
 
+    pub fn is_disjunct(&self) -> bool {
+        matches!(self, Hir::Disjunct(..))
+    }
+
+    pub fn is_concat(&self) -> bool {
+        matches!(self, Hir::Concat(..))
+    }
+
+    pub fn is_repeat(&self) -> bool {
+        matches!(self, Hir::Repeat(..))
+    }
+
+    pub fn is_class(&self) -> bool {
+        matches!(self, Hir::Class(..))
+    }
+
+    pub fn is_literal(&self) -> bool {
+        matches!(self, Hir::Literal(..))
+    }
+
     /// Returns the bounds of the Hir's length. `None` means infinite.
     pub fn len_hint(&self) -> (usize, Option<usize>) {
         match self {
@@ -92,6 +113,13 @@ impl Hir {
             Hir::Literal(bytes) => (bytes.len(), Some(bytes.len())),
         }
     }
+
+    /// Returns `Some(len)` if this hir instance has the exact length, otherwise
+    /// returns `None`.
+    pub fn exact_len(&self) -> Option<usize> {
+        let (lower, upper) = self.len_hint();
+        if Some(lower) == upper { upper } else { None }
+    }
 }
 
 pub struct DisjunctHir {
@@ -110,4 +138,57 @@ pub struct RepeatHir {
     pub min: usize,
     pub max: Option<usize>,
     pub item: Box<Hir>,
+}
+
+impl std::fmt::Display for Hir {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Hir::Literal(bytes) => {
+                for byte in bytes {
+                    std::fmt::Display::fmt(&byte.display(), f)?;
+                }
+            }
+            Hir::Class(set) => {
+                f.write_char('[')?;
+                for range in set.ranges() {
+                    std::fmt::Display::fmt(&range.display(), f)?;
+                }
+                f.write_char(']')?;
+            }
+            Hir::Repeat(repeat) => {
+                let item = &repeat.item;
+                let needs_parens =
+                    !item.is_class() && !item.is_repeat() && item.exact_len() != Some(1);
+                if needs_parens {
+                    f.write_char('(')?;
+                }
+                std::fmt::Display::fmt(&item, f)?;
+                if needs_parens {
+                    f.write_char(')')?;
+                }
+                match (repeat.min, repeat.max) {
+                    (0, None) => f.write_char('*')?,
+                    (1, None) => f.write_char('+')?,
+                    (0, Some(1)) => f.write_char('?')?,
+                    (lower, None) => write!(f, "{{{lower},}}")?,
+                    (lower, Some(upper)) => write!(f, "{{{lower},{upper}}}")?,
+                }
+            }
+            Hir::Concat(concat) => {
+                for item in &concat.items {
+                    std::fmt::Display::fmt(item, f)?;
+                }
+            }
+            Hir::Disjunct(disjunct) => {
+                let alters = &disjunct.alters;
+                for i in 0..alters.len() {
+                    if i + 1 < alters.len() {
+                        f.write_char('|')?;
+                    }
+                    std::fmt::Display::fmt(&alters[i], f)?;
+                }
+            }
+        }
+        Ok(())
+    }
 }
